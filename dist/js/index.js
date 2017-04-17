40c19af934dde0aef74a6d94148f178263b08928
xiao si <rsl1400@gmail.com>
2017-04-17 16:23:18 +0800

    first commits

---
+++ dist/js/index.js
@@ -0,0 +1,1 @@
+$(document).ready(function(){$(".footer").find("li").click(function(){$(this).text()})});
---
+++ src/script/app.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+	$(".footer").find("li").click(function(){
+		var str = $(this).text();
+		if(str === "待办"){
+			//todo 链接数据库
+		}
+		else if(str === "已完成"){
+			//todo
+		}
+		else{
+			//todo
+		}
+	})
+});
